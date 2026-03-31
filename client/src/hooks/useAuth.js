fbc5704316ce39ef1cc2c1741ecc6f8fe257bd22
pratikshakadam912 <pratiksha1177@gmail.com>
2026-03-31 22:17:13 +0530

    firebase auth solved

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../firebase/firebaseConfig";
+import { auth } from "../firebase/firebaseConfig";
 
 
 const useAuth = () => {
