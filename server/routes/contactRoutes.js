f097853eff4b7f6a63c956cb5770177bc8f85f5a
pratikshakadam912 <pratiksha1177@gmail.com>
2026-06-24 22:16:43 +0530

    Routes updated

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,41 +1,9 @@
 import express from "express";
 import Contact from "../models/Contact.js";
-import sendEmail from "../utils/sendEmail.js";
+// import sendEmail from "../utils/sendEmail.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-    try {
-        const { name, email, subject, message } = req.body;
-
-        const contact = await Contact.create({
-            name,
-            email,
-            subject,
-            message,
-        });
-
-        await sendEmail(
-            name,
-            email,
-            subject,
-            message
-        );
-
-        res.status(201).json({
-            success: true,
-            contact,
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
-    }
-});
 router.post("/", async (req, res) => {
     try {
         console.log("Contact request received");
@@ -51,14 +19,11 @@ router.post("/", async (req, res) => {
 
         console.log("Saved to MongoDB");
 
-        await sendEmail(name, email, subject, message);
-
-        console.log("Email sent");
-
         res.status(201).json({
             success: true,
             contact,
         });
+
     } catch (error) {
         console.error("CONTACT ERROR:", error);
 
@@ -68,4 +33,5 @@ router.post("/", async (req, res) => {
         });
     }
 });
+
 export default router;
