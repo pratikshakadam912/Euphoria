ff8dd755991dc6935213b7cfb32488ced73b0742
pratikshakadam912 <pratiksha1177@gmail.com>
2026-06-25 14:12:09 +0530

    deployment error

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (name, email, subject, message) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
