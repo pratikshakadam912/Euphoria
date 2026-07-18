71236c31bc5f4198b23a082d99b79a1c41a6dc01
pratikshakadam912 <pratiksha1177@gmail.com>
2026-07-19 00:02:59 +0530

    controllers updated

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,29 +1,78 @@
 import Product from "../models/Product.js";
 
-// CREATE
+// Create Product
 export const createProduct = async (req, res) => {
+  try {
     const product = await Product.create(req.body);
-    res.json(product);
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-// GET ALL
+// Get All Products
 export const getProducts = async (req, res) => {
-    const products = await Product.find();
+  try {
+    const products = await Product.find().sort({
+      createdAt: -1,
+    });
+
     res.json(products);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-// UPDATE
-export const updateProduct = async (req, res) => {
-    const product = await Product.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-    );
+// Get Single Product
+export const getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
     res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-// DELETE
+// Update Product
+export const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// Delete Product
 export const deleteProduct = async (req, res) => {
+  try {
     await Product.findByIdAndDelete(req.params.id);
-    res.json({ message: "Deleted" });
+
+    res.json({
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
