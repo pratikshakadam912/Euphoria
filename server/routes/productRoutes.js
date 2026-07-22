7ed3971a049908fe9c914614ca0459641ea5d27c
pratikshakadam912 <pratiksha1177@gmail.com>
2026-07-22 23:33:44 +0530

    updating productRountes so it can browse img

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -8,16 +8,23 @@ import {
   deleteProduct,
 } from "../controllers/productController.js";
 
+import upload from "../utils/upload.js";
+
 const router = express.Router();
 
-router.post("/", createProduct);
+// Create Product with Image Upload
+router.post("/", upload.array("images", 10), createProduct);
 
+// Get All Products
 router.get("/", getProducts);
 
+// Get Single Product
 router.get("/:id", getProduct);
 
+// Update Product
 router.put("/:id", updateProduct);
 
+// Delete Product
 router.delete("/:id", deleteProduct);
 
 export default router;
