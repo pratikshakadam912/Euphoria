1523af6b262679694c974428ff209d988a8c4c44
pratikshakadam912 <pratiksha1177@gmail.com>
2026-09-19 21:34:58 +0530

    auto scrolls to top

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";  
+import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
 import "./index.css";
 
 import { CartProvider } from "./context/CartContext";
-
+import ScrollToTop from "./components/common/ScrollToTop";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <CartProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} />
       </CartProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
