c46a62ab8664a0f597dc8cc783eb952b1dc6b7c7
pratikshakadam912 <pratiksha1177@gmail.com>
2026-09-25 13:25:16 +0530

    logout button is working

--- client/src/admin/components/Navbar.jsx
+++ client/src/admin/components/Navbar.jsx
@@ -1,12 +1,31 @@
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/firebaseConfig";
+
 const Navbar = () => {
-    return (
-        <div className="bg-white shadow p-4 flex justify-between items-center">
-            <h2 className="text-xl font-semibold">Admin Dashboard</h2>
-            <button className="bg-black text-white px-4 py-2 rounded">
-                Logout
-            </button>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  };
+
+  return (
+    <div className="bg-white shadow p-4 flex justify-between items-center">
+      <h2 className="text-xl font-semibold">Admin Dashboard</h2>
+
+      <button
+        onClick={handleLogout}
+        className="bg-black text-white px-4 py-2 rounded"
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
